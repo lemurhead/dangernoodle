eed6af32f8795fef3805c84ce93bef2339ec13e6
lemurhead <lemurheadi@gmail.com>
2025-04-15 17:49:17 -0700

    loopopoypdsfds

---
+++ loops.py
@@ -0,0 +1,10 @@
+for x in range(0, 8):
+    print('hello %s' % x)
+    
+x = 0 
+print('hello %s' % x)
+
+wizard_list = ['spider legs', 'toe of frog', 'snail tongue',
+'bat wing', 'slug butter', 'bear burp']
+for i in wizard_list:
+    print(i)
