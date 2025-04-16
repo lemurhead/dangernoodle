b67847c9883ce91399e522f68936750f5a4d670d
lemurhead <lemurheadi@gmail.com>
2025-04-15 17:17:13 -0700

    chptr 5

---
+++ ifstate.py
@@ -0,0 +1,49 @@
+age = 20
+if age >= 12:
+    print("old man")
+else:
+    print("young man")
+    
+if age == 20 or age == 23 or age == 24:
+    print('go to college')
+elif age == 21:
+    print('go drinking and driving')
+elif age >= 19 and age >=18:
+    ('go to school')
+else:
+    print('skibidi sigma')
+
+val = None
+if val == None:
+    print('val has no value')
+    
+Age = '10'
+converted_age = int(Age)
+converted_age = int(Age)
+if converted_age == 10:
+    print('yay stored in a string')
+    
+lily_stinky_factor = '10.5'
+stinky_factor = float(lily_stinky_factor)
+print(stinky_factor)
+
+twinkies = 100
+if twinkies <= 100:
+    print("too few")
+elif twinkies >= 500:
+    print("too many")
+    
+money = 200
+if money >= 100 or money <= 500:
+    print("acceptable value")
+else:
+    print("unacceptable")
+    
+ninjas = 49
+if ninjas >= 50:
+    print("thats too many")
+elif ninjas >= 30:
+    print("it'll be a struggle but i can fight them")
+elif ninjas >= 10:
+    print("I can fight those ninjas!")
+    
