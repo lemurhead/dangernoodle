74346ba419e171d307f6e9c5e3df209c9068cd32
YOUR_NAME <YOUR_EMAIL>
2025-07-25 12:00:22 -0700

    madee gooome oveer screen.

--- pong.py
+++ pong.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import random
 import time
+
 tk = Tk()
 tk.title("Game")
 tk.resizable(0, 0)
@@ -9,39 +10,6 @@ canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
 canvas.pack()
 tk.update()
 
-class Ball:
-    def __init__(self, canvas, paddle, color):
-        self.canvas = canvas
-        self.paddle = paddle
-        self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
-        self.canvas.move(self.id,245,100)
-        starts = [-3, -2, -1, 1, 2, 3]
-        random.shuffle(starts)
-        self.x = starts[0]
-        self.y = -3
-        self.canvas_height = self.canvas.winfo_height()
-        self.canvas_width = self.canvas.winfo_width()
-        self.hit_bottom = False
-    def hit_paddle(self, pos):
-        paddle_pos = self.canvas.coords(self.paddle.id)
-        if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
-            if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
-                return True
-        return False
-    def draw(self):
-        self.canvas.move(self.id, self.x, self.y)
-        pos = self.canvas.coords(self.id)
-        if pos[1] <= 0:
-            self.y = 1
-        if pos[3] >= self.canvas_height:
-            self.hit_bottom = True
-        if self.hit_paddle(pos) == True:
-            self.y = -3
-        if pos[0] <= 0:
-            self.x = 3
-        if pos[2] >= self.canvas_width:
-            self.x = -3
-            
 class Paddle:
     def __init__(self, canvas, color):
         self.canvas = canvas
@@ -51,36 +19,62 @@ class Paddle:
         self.canvas_width = self.canvas.winfo_width()
         self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
         self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
+
     def draw(self):
         self.canvas.move(self.id, self.x, 0)
         pos = self.canvas.coords(self.id)
-        if pos[0] <= 0:
+        if pos[0] <= 0 or pos[2] >= self.canvas_width:
             self.x = 0
-        elif pos[2] >= self.canvas_width:
-            self.x = 0
+
     def turn_left(self, evt):
         self.x = -2
+
     def turn_right(self, evt):
-        self.x = 2        
+        self.x = 2
+
+class Ball:
+    def __init__(self, canvas, paddle, color):
+        self.canvas = canvas
+        self.paddle = paddle
+        self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
+        self.canvas.move(self.id, 245, 100)
+        starts = [-3, -2, -1, 1, 2, 3]
+        random.shuffle(starts)
+        self.x = starts[0]
+        self.y = -3
+        self.canvas_height = self.canvas.winfo_height()
+        self.canvas_width = self.canvas.winfo_width()
+        self.hit_bottom = False
+
+    def hit_paddle(self, pos):
+        paddle_pos = self.canvas.coords(self.paddle.id)
+        if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
+            if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
+                return True
+        return False
+
+    def draw(self):
+        self.canvas.move(self.id, self.x, self.y)
+        pos = self.canvas.coords(self.id)
+
+        if pos[1] <= 0:
+            self.y = 3
+        if pos[3] >= self.canvas_height:
+            self.hit_bottom = True
+        if self.hit_paddle(pos):
+            self.y = -3
+        if pos[0] <= 0:
+            self.x = 3
+        if pos[2] >= self.canvas_width:
+            self.x = -3
 
 paddle = Paddle(canvas, 'red')
 ball = Ball(canvas, paddle, 'green')
 
-
 while 1:
-    if ball.hit_bottom == False:
+    if not ball.hit_bottom:
         ball.draw()
-        paddle.draw() 
-    else:
-        canvas.create_text(
-            250, 200,  # Center of canvas
-            text="GAME OVER",
-            font=('Helvetica', 30),
-            fill='red'
-            
-        )
-        break
-tk.update_idletasks()
-tk.update()
-time.sleep(0.01)
-
+        paddle.draw()
+    tk.update_idletasks()
+    tk.update()
+    time.sleep(0.01)
