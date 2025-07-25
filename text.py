5c93bbfb85f2a8c821f49acf7b68e2e03288b5e8
YOUR_NAME <you@example.com>
2025-07-24 20:57:41 -0700

    new game

--- text.py
+++ text.py
@@ -1,5 +1,5 @@
 from tkinter import *
 tk = Tk()
-canvas = Canvas(tk, width 500, height 500)
+canvas = Canvas(tk, width=500, height=500)
 canvas.pack()
-canvas.create_text(150, 100 text='lily is super short')
+canvas.create_text(150, 100, text='lily is super short')
